ce9581b21d212c595a7e32fa590d795a8d720324
adskidolen <alex_speedy@mail.bg>
2018-12-02 17:49:23 +0200

    Implemented Order entity model

---
+++ src/KeepHome.Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace KeepHome.Models
+{
+    using System;
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public string CustomerId { get; set; }
+        public virtual KeepHomeUser Customer { get; set; }
+    }
+}
