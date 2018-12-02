007c16e830b4085ac80a2f0ab68cf25fd5fb15b9
polmaxbg <viktorslavchev@abv.bg>
2018-12-02 17:15:37 +0200

    kjk

---
+++ src/KeepHome.Models/Town.cs
@@ -0,0 +1,17 @@
+﻿
+namespace KeepHome.Models
+{
+    using System.Collections.Generic;
+    public class Town
+    {
+        public Town()
+        {
+            this.Users = new HashSet<KeepHomeUser>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<KeepHomeUser> Users { get; set; }
+
+    }
+}
