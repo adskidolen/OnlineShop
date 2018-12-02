b038bb8b6bff8973d7c6f84b736c83a4a7d4ca90
adskidolen <alex_speedy@mail.bg>
2018-12-02 17:41:25 +0200

    Implemented Order entity model

--- src/KeepHome.Models/Country.cs
+++ src/KeepHome.Models/Country.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace KeepHome.Models
+﻿namespace KeepHome.Models
 {
+    using System.Collections.Generic;
+
     public class Country
     {
         public Country()
         {
             this.Towns = new HashSet<Town>();
         }
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public virtual ICollection<Town> Towns{ get; set; }
+        public virtual ICollection<Town> Towns { get; set; }
     }
 }
