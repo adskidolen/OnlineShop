1e8023242e23a79db3378caf08853e388586108f
adskidolen <alex_speedy@mail.bg>
2019-01-29 00:50:57 +0200

    Favicon added

---
+++ src/KeepHome.Web/Controllers/ParentCategoriesController.cs
@@ -0,0 +1,42 @@
+﻿namespace KeepHome.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using KeepHome.Services.Contracts;
+    using KeepHome.Web.Controllers.Base;
+    using KeepHome.Web.ViewModels;
+    using KeepHome.Web.ViewModels.ParentCategories;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ParentCategoriesController : BaseController
+    {
+        private readonly IParentCategoryService parentCategoryService;
+        private readonly IMapper mapper;
+
+        public ParentCategoriesController(IParentCategoryService parentCategoryService, IMapper mapper)
+        {
+            this.parentCategoryService = parentCategoryService;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Details(int id)
+        {
+            var category = this.parentCategoryService.GetCategoryById(id);
+
+            if (category == null)
+            {
+                return this.View(new ErrorViewModel { RequestId = "Invalid category!" });
+            }
+
+            var viewModel = this.mapper.Map<ParentCategoryViewModel>(category);
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ src/KeepHome.Web/ViewModels/ParentCategories/ParentCategoryPartialViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KeepHome.Web.ViewModels.ParentCategories
+{
+    public class ParentCategoryPartialViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
